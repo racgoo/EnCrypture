aac6af7c49a22a8567a6053221c9e52dfbca1a1d
racgoo <lhsung98@naver.com>
2025-09-13 19:54:54 +0900

    chore(Router): setup react-router-dom as provider

---
+++ src/app/provider/route/route.provider.tsx
@@ -0,0 +1,21 @@
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import HomePage from "../../../pages/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" replace={true} />,
+  },
+]);
+
+export function RoutesProvider() {
+  return <RouterProvider router={router} />;
+}
